e39924ae3e4f809c675a873acf8c9774b8fba7a7
Bagas Tri Hadmodjo S.Kom <Bagastrihadmodjo@gmail.com>
2026-05-24 12:46:46 +0700

    Add files via upload

---
+++ blog-bth/script.js
@@ -0,0 +1,44 @@
+// TIPE DATA ARRAY dan PENGGUNAAN BACKTICK
+// document.writeln('Hi, my name is Bagas Tri Hadmodjo S.Kom');
+
+// let firstName = "Bagas";
+// let middleName = "Tri";
+// let lastName = "Hadmodjo";
+
+// let fullName = firstName + ' ' + middleName + ' ' + lastName;
+// document.writeln('Hi, my name is ' + fullName);
+
+
+
+// STRING TEMPLATE MULTILINES
+// let names = [];
+// names.push('Bagas');
+// names.push('Tri', 'Hadmodjo');
+// names.pop(1);
+
+// // document.writeln("<pre>");
+// console.log(names);
+// // document.writeln("</pre>");
+
+// POPUP ALERT, PROMPT dan CONFIRM
+// const masuk = confirm("Apakah anda ingin mengunjungi Blog BTH ?") 
+// 	if(masuk) {
+// 		const name = prompt("Tuliskan nama Anda")
+// 		alert(`Selamat datang di Blog BTH kak ${name}`)
+// 	} else {
+// 		alert("Bye bye 😁👌")
+// 	};
+
+
+// TERNARY OPERATOR
+// const nilai = 75;
+// let ucapan;
+
+// ucapan = nilai >= 90 ? "Selamat Anda Lulus" : "Maaf , Anda tidak lulus";
+// document.writeln(`<p>${ucapan}</p>`);
+
+
+// FOR LOOP
+// for (x = 1; x <= 10;x++) {
+// 	document.writeln(`Perulangan ke ${x} <hr>`)
+// };
